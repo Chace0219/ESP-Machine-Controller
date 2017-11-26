e3435a969fdf68850d986551e06b1e5f1b5916f7
DESKTOP-U0UGAT3\Embedded <2435575291@qq.com>
2017-11-26 04:18:48 -0800

    version 1.5

---
+++ controller/NexHotspot.cpp
@@ -0,0 +1,13 @@
+/*
+Library is Patched by yangweixian@cybergal.com
+Date : 2017.10
+*/
+
+ 
+#include "NexHotspot.h"
+
+NexHotspot::NexHotspot(uint8_t pid, uint8_t cid, const char *name)
+    :NexTouch(pid, cid, name)
+{
+}
+
